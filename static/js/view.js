ff4f819abe4ffa416518051fef8701195fc5d594
uetzau01 <uetzau01@luther.edu>
2018-04-11 21:03:41 -0500

    resolving conflicts

--- static/js/view.js
+++ static/js/view.js
@@ -72,6 +72,9 @@ class GamesView {
 							let humidity = obs.hum
 							let windSpeed = obs.wspdi
 							let windDire = obs.wdire
+							if (gameArray[i].city == "Old_Toronto") {
+								gameArray[i].city = "Toronto"
+							}
 
 		            		contentString += '<h3>' + gameArray[i].city + ', ' + gameArray[i].state + 
 		            		'<span id="time">' + localTime.toLocaleString() + '</span></h3>' +
@@ -99,37 +102,40 @@ class GamesView {
 								return response.json()
 						})
 						.then(function(data){
-								console.log(data)
-								let todaysdate = new Date()
-								let difference = Math.round((localTime.getTime() - todaysdate.getTime()) / 3600000)
-								if (difference < 0) {
-									difference = 0;
-								}
-								console.log(difference) 
-								//data.hourly_forecast 
-								let obs = data.hourly_forecast[difference]
-								let temp = obs.temp.english
-								let conditions = obs.condition
-								let humidity = obs.humidity
-								let windSpeed = obs.wspd.english
-								let windDire = obs.wdir.dir
+							console.log(data)
+							let todaysdate = new Date()
+							let difference = Math.round((localTime.getTime() - todaysdate.getTime()) / 3600000)
+							if (difference < 0) {
+								difference = 0;
+							}
+							console.log(difference) 
+							//data.hourly_forecast 
+							let obs = data.hourly_forecast[difference]
+							let temp = obs.temp.english
+							let conditions = obs.condition
+							let humidity = obs.humidity
+							let windSpeed = obs.wspd.english
+							let windDire = obs.wdir.dir
+							if (gameArray[i].city == "Old_Toronto") {
+								gameArray[i].city = "Toronto"
+							}
 
-								contentString += '<h3>' + gameArray[i].city + ', ' + gameArray[i].state + 
-			            		'<span id="time">' + localTime.toLocaleString() + '</span></h3>' +
-			            		'<p id="gameScoreOutput">' + gameArray[i].homeTeam + ' vs. ' + gameArray[i].awayTeam + '</p></div>' + 
-			            		'<div class="column">'  + 
-			            		'<div class="row"><img src="../images/temp.png">' + 
-			            		'<span>Temp: ' + temp + '&#8457</span></div>' + 
-			            		'<div class="row"><img src="../images/wind.png">' +
-			            		'<span>Wind: ' + windSpeed + ' mph ' + windDire + '</span></div></div>' + 
-			            		'<div class="column">' +
-			            		'<div class="row"><img src="../images/conditions.png">' + 
-			            		'<span>' + conditions + '</span></div>' + 
-			            		'<div class="row"><img src="../images/humidity.png">' +
-			            		'<span>Humidity: ' + humidity + '%</span></div></div>'
+							contentString += '<h3>' + gameArray[i].city + ', ' + gameArray[i].state + 
+		            		'<span id="time">' + localTime.toLocaleString() + '</span></h3>' +
+		            		'<p id="gameScoreOutput">' + gameArray[i].homeTeam + ' vs. ' + gameArray[i].awayTeam + '</p></div>' + 
+		            		'<div class="column">'  + 
+		            		'<div class="row"><img src="../images/temp.png">' + 
+		            		'<span>Temp: ' + temp + '&#8457</span></div>' + 
+		            		'<div class="row"><img src="../images/wind.png">' +
+		            		'<span>Wind: ' + windSpeed + ' mph ' + windDire + '</span></div></div>' + 
+		            		'<div class="column">' +
+		            		'<div class="row"><img src="../images/conditions.png">' + 
+		            		'<span>' + conditions + '</span></div>' + 
+		            		'<div class="row"><img src="../images/humidity.png">' +
+		            		'<span>Humidity: ' + humidity + '%</span></div></div>'
 
-								infowindow.setContent(contentString);
-			            		infowindow.open(map, marker);
+							infowindow.setContent(contentString);
+		            		infowindow.open(map, marker);
 						})
 		        	}
 		        }
